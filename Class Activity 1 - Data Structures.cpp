92d162d207bf246a052d90be938c1c0f33bcf34c
J <J@JULS-PC>
2023-01-14 11:20:57 -0600

    Add project files.

---
+++ Class Activity 1 - Data Structures.cpp
@@ -0,0 +1,17 @@
+// Class Activity 1 - Data Structures.cpp
+// Julia Pletcher-Arnold
+// COMS2213
+// January 14, 2023
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Welcome to Spring 2023 at ATU" << endl;
+
+    return 0;
+
+}
+
